c59a6d3521fde3ec0f62ba6dcb87f396358c7647
Rustam-Fozilov <fozilovrustam32@gmail.com>
2024-05-18 11:53:50 +0500

    fresh setup

---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="h-screen w-full bg-black">
+    <h1 class="text-white">THIS IS MAIN PAGE</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+
+</script>
---
+++ plugins/locally.ts
@@ -0,0 +1,21 @@
+export default defineNuxtPlugin(() => {
+  return {
+    provide: {
+      getItem(key: string) {
+        if (process.client) {
+          return localStorage.getItem(key)
+        } else {
+          return undefined
+        }
+      },
+
+      setItem(key: string, value: any) {
+        if (process.client) {
+          return localStorage.setItem(key, value);
+        } else {
+          return undefined
+        }
+      }
+    }
+  }
+})
