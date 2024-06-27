d2ec60d9924e6f9d05a65b5ae7cc9aa0f32948ce
Rustam-Fozilov <fozilovrustam32@gmail.com>
2024-06-27 22:27:59 +0500

    in process: univers page 🚀

--- components/modules/TopUniversities.vue
+++ components/modules/TopUniversities.vue
@@ -1,8 +1,13 @@
 <template>
   <div>
     <div class="flex flex-col gap-12">
-      <div class="title">
-        Top universitetlar
+      <div class="flex justify-between items-end">
+        <div class="title">
+          Top universitetlar
+        </div>
+        <a href="/universities/" class="hover:underline">
+          Barchasini ko'rish
+        </a>
       </div>
 
       <div class="flex justify-start gap-5">
---
+++ pages/universities/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <div class="my-container">
+      <div class="py-24 flex justify-between gap-5">
+        <div class="title w-1/2">
+          <span class="font-tt-medium">Universitetlar. </span>
+          <span class="opacity-50">Amerika orzuingizni amalga oshiring</span>
+        </div>
+        <div class="w-1/2">
+          <div class="flex justify-end gap-5">
+            <div class="px-10 py-5 rounded-2xl bg-white inline-block">
+              <div class="title">100+</div>
+              <div class="opacity-50 sub-title">Universitetlar</div>
+            </div>
+            <div class="px-10 py-5 rounded-2xl bg-white inline-block">
+              <div class="title">50+</div>
+              <div class="opacity-50 sub-title">Shaxarlar</div>
+            </div>
+            <div class="px-10 py-5 rounded-2xl bg-white inline-block">
+              <div class="title">40+</div>
+              <div class="opacity-50 sub-title">Yo'nalishlar</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="bg-white">
+      <div class="pt-24 my-container">
+        fdas
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+definePageMeta({
+  layout: "main-layout"
+})
+
+</script>
