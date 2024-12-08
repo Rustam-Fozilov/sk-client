4ff5f43709e46afc79df0f99acc3ac9a9b74c656
Rustam Fozilov <thisisrustam@gmail.com>
2024-12-08 12:45:33 +0500

    done contact page mobile

--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -1,46 +1,47 @@
 <template>
   <div class="my-container">
-    <div class="mt-12">
-      <div class="title">/ Kontaktlar</div>
+    <div class="mt-12 md:mt-5">
+      <div class="title sm:mid-title">/ Kontaktlar</div>
     </div>
-    <div class="flex gap-12 items-center my-12">
+    <div class="flex gap-12 items-center my-12 md:my-5 md:flex-col md:items-start md:gap-5">
       <div>
-        <div class="font-tt-medium text-sm">thisissk@gmail.com</div>
+        <div class="font-tt-medium text-sm sm:text-rg">thisissk@gmail.com</div>
         <div class="opacity-50">Umumiy savollar uchun</div>
       </div>
 
       <div>
-        <div class="font-tt-medium text-sm">t.me/@skbot</div>
+        <div class="font-tt-medium text-sm sm:text-rg">t.me/@skbot</div>
         <div class="opacity-50">Taklif va shikoyatlar uchun</div>
       </div>
 
       <div>
-        <div class="font-tt-medium text-sm">t.me/@skprivat</div>
+        <div class="font-tt-medium text-sm sm:text-rg">t.me/@skprivat</div>
         <div class="opacity-50">Shaxsiy chat</div>
       </div>
 
       <div>
-        <div class="font-tt-medium text-sm">t.me/@skcommunity</div>
+        <div class="font-tt-medium text-sm sm:text-rg">t.me/@skcommunity</div>
         <div class="opacity-50">Kamyuniti</div>
       </div>
     </div>
 
-    <div class="bg-white p-12 rounded-2xl flex justify-between items-start">
+    <div class="bg-white p-12 sm:p-6 rounded-2xl flex justify-between items-start lg:flex-col lg:gap-5">
       <div>
-        <div class="mid-title">Biz bilan bog'laning</div>
-        <div class="w-[25rem] mt-7 leading-none">Вы можете написать нам по любому интересующему вас вопросу: от обучения до сотрудничества</div>
+        <div class="mid-title sm:text-lg">Biz bilan bog'laning</div>
+        <div class="w-[25rem] mt-7 sm:mt-4 leading-none sm:w-full">Вы можете написать нам по любому интересующему вас вопросу: от обучения до сотрудничества</div>
       </div>
 
-      <form action="">
-        <div class="w-[420px]">
+      <form action="" class="md:w-full">
+        <div class="w-[420px] lg:w-full">
           <div class="bg-soft-white border border-black border-opacity-10 py-3 px-5 rounded-lg w-full">
             <input required type="text" placeholder="Ism" class="bg-transparent w-full outline-none">
           </div>
-          <div class="flex justify-between">
-            <div class="bg-soft-white border border-black border-opacity-10 py-3 px-5 rounded-lg mt-5">
-              <input required maxlength="13" type="phone" placeholder="+998" class="bg-transparent w-full outline-none">
+          <div class="flex justify-between sm:flex-col sm:justify-start sm:items-start">
+            <div class="bg-soft-white border flex border-black border-opacity-10 md:w-[48%] sm:w-full py-3 px-5 rounded-lg mt-5">
+              <div>+998</div>
+              <input required maxlength="9" type="phone" class="bg-transparent w-full outline-none">
             </div>
-            <div class="bg-soft-white border border-black border-opacity-10 py-3 px-5 rounded-lg mt-5 ml-5">
+            <div class="bg-soft-white border border-black border-opacity-10 md:w-[48%] sm:w-full py-3 px-5 rounded-lg mt-5 ml-5 sm:ml-0">
               <input required type="email" placeholder="Email" class="bg-transparent w-full outline-none">
             </div>
           </div>
