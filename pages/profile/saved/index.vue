7d0a03c7cf5f095c634cf124a9e4a2a506c2fdda
Rustam Fozilov <thisisrustam@gmail.com>
2024-12-08 11:43:03 +0500

    done profile saved page mobile

--- pages/profile/saved/index.vue
+++ pages/profile/saved/index.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="my-container">
-        <div class="text-2xl font-tt-medium border-b border-b-black border-opacity-10 py-10">
+        <div class="text-2xl font-tt-medium border-b border-b-black border-opacity-10 py-10 lg:py-5 md:py-3 md:text-lg">
             <div>Salom Rustam</div>
         </div>
 
-        <div class="mt-12">
+        <div class="mt-12 md:mt-5">
             <university-list />
         </div>
     </div>
